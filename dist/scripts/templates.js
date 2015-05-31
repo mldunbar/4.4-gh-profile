6a5adaa7fad2cc65ee2e0437caca8cc27275b9d3
Megan Dunbar <meganleedunbar@gmail.com>
2015-05-30 23:07:03 -0400

    tweaked CSS

--- dist/scripts/templates.js
+++ dist/scripts/templates.js
@@ -5,9 +5,9 @@ this["JST"]["application"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1
 this["JST"]["repo-template"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<div class=\"repo-template\">\n  <h2 class=\"repo-name\">"
+  return "<div class=\"repo-template\">\n  <a href=\"#\">"
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
-    + "</h2>\n  <h4>Joined at"
+    + "</a>\n  <h4>This is connected"
     + alias3(((helper = (helper = helpers.updated_at || (depth0 != null ? depth0.updated_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"updated_at","hash":{},"data":data}) : helper)))
     + "</h4>\n  <h3>"
     + alias3(((helper = (helper = helpers.language || (depth0 != null ? depth0.language : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"language","hash":{},"data":data}) : helper)))
@@ -28,17 +28,17 @@ this["JST"]["user-template"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta
     + alias3(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"name","hash":{},"data":data}) : helper)))
     + "</h2>\n  <h3 class=\"login\">"
     + alias3(((helper = (helper = helpers.login || (depth0 != null ? depth0.login : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"login","hash":{},"data":data}) : helper)))
-    + "</h3>\n</div>\n<div class=\"attributes\">\n  <span class=\"octicon octicon-location\"></span>\n  <h6>"
+    + "</h3>\n</div>\n<ul class=\"attributes\">\n  <li>\n    <span class=\"octicon octicon-location\"></span>\n    <h6 class=\"location\">"
     + alias3(((helper = (helper = helpers.location || (depth0 != null ? depth0.location : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"location","hash":{},"data":data}) : helper)))
-    + "</h6>\n  <span class=\"octicon octicon-clock\"></span>\n  <h6>"
+    + "</h6>\n  </li>\n  <li>\n    <span class=\"octicon octicon-clock\"></span>\n    <h6>Joined on "
     + alias3(((helper = (helper = helpers.created_at || (depth0 != null ? depth0.created_at : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"created_at","hash":{},"data":data}) : helper)))
-    + "</h6>\n</div>\n\n<ul>\n  <li>"
+    + "</h6>\n  </li>\n</ul>\n\n<ul class=\"numbers\">\n  <li class=\"followers\">"
     + alias3(((helper = (helper = helpers.followers || (depth0 != null ? depth0.followers : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"followers","hash":{},"data":data}) : helper)))
-    + "</li>\n  <li>"
+    + "\n    <span>Followers</span>\n  </li>\n  <li class=\"starred\">"
     + alias3(((helper = (helper = helpers.starred || (depth0 != null ? depth0.starred : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"starred","hash":{},"data":data}) : helper)))
-    + "</li>\n  <li>"
+    + "\n    <span>Starred</span>\n  </li>\n  <li class=\"following\">"
     + alias3(((helper = (helper = helpers.following || (depth0 != null ? depth0.following : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"following","hash":{},"data":data}) : helper)))
-    + "</li>\n</ul>\n\n\n<h4>Organizations</h4>\n<img src=\""
+    + "\n    <span>Following</span>\n  </li>\n</ul>\n\n\n<h4>Organizations</h4>\n<img src=\""
     + alias3(((helper = (helper = helpers.organizations_url || (depth0 != null ? depth0.organizations_url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"organizations_url","hash":{},"data":data}) : helper)))
     + "\" alt=\""
     + alias3(((helper = (helper = helpers['y u no work'] || (depth0 != null ? depth0['y u no work'] : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"y u no work","hash":{},"data":data}) : helper)))
